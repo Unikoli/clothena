585a6b9dcb3edcdfe8096b998f5e9dc47780c13b
Unikoli <uniqueoli16@gmail.com>
2025-05-02 10:18:06 +0545

    authentication middleware added

---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    // Expected format: Bearer <token>
+    try {
+        const token = authHeader && authHeader.split(' ')[1];
+  
+    if (!token) {
+      return res.status(401).json({ message: 'Access token missing' });
+    }
+  
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(403).json({ message: 'Invalid or expired token' });
+      }
+  
+      // Save user info in request for use in next middleware or route
+      req.user = decoded;
+      next();
+    });
+    } catch (err) {
+        res.status(500).json({error:err.message})
+    }
+    
+  };
+  
+  module.exports = authenticateToken;
+  
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const { register, getUsers, login } = require("../controllers/auth.controller");
+const authenticateToken = require("../middlewares/auth.middleware");
 const router=express.Router();
 
 router.post('/login',login);
