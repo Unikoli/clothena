5ff7b3d743b174c5cdb76ce8cca3cae1f27e5c8a
Unikoli <uniqueoli16@gmail.com>
2025-05-18 14:39:42 +0545

    server.js modified,network allows all the ip

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
 const PORT=process.env.PORT||5000;
-const server=app.listen(PORT,()=>{
+const server=app.listen(PORT,'0.0.0.0',()=>{
     console.log(`server is running on port ${PORT}`);
 })
 
